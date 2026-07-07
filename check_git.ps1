d3456c3b54f5af4b10ed543e60b27d6bda7a576f
PanSomeone <pansomeone927@outlook.com>
2026-07-07 09:59:55 +0800

    修改了yml配置

---
+++ check_git.ps1
@@ -0,0 +1,26 @@
+Set-Location "C:/Users/panso/Desktop/movie/movie_view"
+$out = @()
+$out += "=== BRANCH ==="
+$out += (git branch --show-current 2>&1)
+$out += ""
+$out += "=== BRANCHES ==="
+$out += (git branch -a 2>&1)
+$out += ""
+$out += "=== REMOTE ==="
+$out += (git remote -v 2>&1)
+$out += ""
+$out += "=== LOG (last 5) ==="
+$out += (git log --oneline -5 2>&1)
+$out += ""
+$out += "=== STATUS ==="
+$out += (git status --short 2>&1)
+$out += ""
+$out += "=== USER ==="
+$out += "name: $(git config user.name 2>&1)"
+$out += "email: $(git config user.email 2>&1)"
+$out += ""
+$out += "=== DIFF STAT ==="
+$out += (git diff --stat HEAD 2>&1)
+
+$out | Out-File -FilePath "C:/Users/panso/Desktop/movie/movie_view/git_status.txt" -Encoding UTF8
+Write-Host "DONE"
